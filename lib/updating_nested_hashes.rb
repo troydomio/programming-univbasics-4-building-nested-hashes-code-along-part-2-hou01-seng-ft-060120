446de4e8f8f88b40aeed3a6fa792fc78b9f991e3
Troy Domio <troyakins99@yahoo.com>
2020-05-13 04:12:37 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -23,10 +23,9 @@ def update_status
    def update_status
      epic_tragedy[:montague][:hero][:status] = "dead"
     epic_tragedy[:capulet][:heroine][:status]= "dead"
-end
-epic_tragedy
-
-end
+    epic_tragedy
+  end
+ end
 
 
 
