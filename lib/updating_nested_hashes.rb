0e71984c489aed883ad914527a94eb207c49e39e
Troy Domio <troyakins99@yahoo.com>
2020-05-13 04:35:59 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -61,11 +61,8 @@ def add_characters
   epic_tragedy[:additional_characters]=[]
   
    
-apothecary={:name => "Prince Escalus"}
-prince= {:name => "Apothecary"}
-
-epic_tragedy[:additional_characters]<< prince
-epic_tragedy[:additional_characters]<< apothecary
+epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
+epic_tragedy[:additional_characters] << { name: "Apothecary" }
 
 return epic_tragedy
 
